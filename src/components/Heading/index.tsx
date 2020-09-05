b02ca694e86b6ae3b73b94570fed5b21305c1f6c
Felipe Jung <felipemattoseu@gmail.com>
2020-09-05 16:41:47 -0400

    chore(heading): adding prop to change line left and line bottom color

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -1,19 +1,31 @@
 import { Wrapper } from './styles';
 
+export type LineColors = 'primary' | 'secondary';
+
 export type HeadingProps = {
   children: string;
   color?: 'white' | 'black';
   lineLeft?: boolean;
   lineBottom?: boolean;
+  lineColor?: LineColors;
+  size?: 'small' | 'medium';
 };
 
 const Heading: React.FC<HeadingProps> = ({
   children,
   color = 'white',
   lineLeft = false,
-  lineBottom = false
+  lineBottom = false,
+  lineColor = 'primary',
+  size = 'medium'
 }) => (
-  <Wrapper color={color} lineLeft={lineLeft} lineBottom={lineBottom}>
+  <Wrapper
+    color={color}
+    lineLeft={lineLeft}
+    lineBottom={lineBottom}
+    lineColor={lineColor}
+    size={size}
+  >
     {children}
   </Wrapper>
 );
