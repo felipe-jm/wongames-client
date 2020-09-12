a082f6945fc401b48bf896af65dc824a4462cccf
Felipe Jung <felipemattoseu@gmail.com>
2020-09-12 19:17:10 -0400

    chore(button): button as link

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,21 +1,34 @@
+import { ButtonHTMLAttributes, AnchorHTMLAttributes } from 'react';
+
 import { Wrapper } from './styles';
 
+type ButtonTypes =
+  | AnchorHTMLAttributes<HTMLAnchorElement>
+  | ButtonHTMLAttributes<HTMLButtonElement>;
+
 export type ButtonProps = {
-  children?: React.ReactNode;
   size?: 'small' | 'medium' | 'large';
   fullWidth?: boolean;
   icon?: JSX.Element;
+  as?: React.ElementType;
   onClick?: () => (event: React.MouseEvent<HTMLButtonElement>) => void;
-};
+} & ButtonTypes;
 
 const Button: React.FC<ButtonProps> = ({
   children,
   icon,
+  as,
   size = 'medium',
   fullWidth = false,
   ...props
 }) => (
-  <Wrapper size={size} fullWidth={fullWidth} hasIcon={!!icon} {...props}>
+  <Wrapper
+    as={as}
+    size={size}
+    fullWidth={fullWidth}
+    hasIcon={!!icon}
+    {...props}
+  >
     {!!icon && icon}
     {!!children && <span>{children}</span>}
   </Wrapper>
