9af59fd4998409f48305a815e2f0583a19a55a61
Felipe Jung <felipemattoseu@gmail.com>
2021-02-14 18:23:17 -0400

    chore(home page): add graphql query to fetch banners

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,36 @@
-import { GetServerSideProps, NextPage } from 'next';
+import { GetStaticProps, NextPage } from 'next';
 
+import { QueryHome } from 'graphql/generated/QueryHome';
+import { QUERY_HOME } from 'graphql/queries/home';
 import Home, { HomeTemplateProps } from 'templates/Home';
 
-import bannersMock from 'components/BannerSlider/mock';
 import gamesMock from 'components/GameCardSlider/mock';
 import HightlightMock from 'components/Highlight/mock';
 
+import { initializeApollo } from 'utils/apollo';
+
 const Index: NextPage<HomeTemplateProps> = (props) => <Home {...props} />;
 
-export const getServerSideProps: GetServerSideProps<HomeTemplateProps> = async () => {
+export const getStaticProps: GetStaticProps = async () => {
+  const apolloClient = initializeApollo();
+
+  const { data } = await apolloClient.query<QueryHome>({ query: QUERY_HOME });
+
   return {
     props: {
-      banners: bannersMock,
+      revalidate: 60,
+      banners: data.banners.map((banner) => ({
+        img: `http://localhost:1337${banner.image?.url}`,
+        title: banner.title,
+        subtitle: banner.subtitle,
+        buttonLabel: banner.button?.label || null,
+        buttonLink: banner.button?.link || null,
+        ...(banner.ribbon && {
+          ribbon: banner.ribbon.text,
+          ribbonColor: banner.ribbon.color,
+          ribbonSize: banner.ribbon.size
+        })
+      })),
       newGames: gamesMock,
       mostPopularHighlight: HightlightMock,
       mostPopularGames: gamesMock,
