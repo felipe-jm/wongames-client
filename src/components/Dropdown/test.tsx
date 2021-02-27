0f8e9aec669c2d0aa5153410c7760b1fd2188279
Felipe Jung <felipemattoseu@gmail.com>
2021-02-27 14:02:59 -0400

    chore(dropdown): add option to close dropdown when clicking outside of it

--- src/components/Dropdown/test.tsx
+++ src/components/Dropdown/test.tsx
@@ -26,4 +26,23 @@ describe('<Dropdown />', () => {
     expect(content).toHaveStyle({ opacity: 1 });
     expect(content.getAttribute('aria-hidden')).toBe('false');
   });
+
+  it('should handle open/close dropdown when clicking on overlay', () => {
+    renderWithTheme(<Dropdown title="Title">Children</Dropdown>);
+
+    const content = screen.getByText(/children/i);
+
+    // element right after content
+    const overlay = content.nextElementSibling;
+
+    userEvent.click(screen.getByText(/title/i));
+
+    expect(overlay).toHaveStyle({ opacity: 1 });
+    expect(overlay!.getAttribute('aria-hidden')).toBe('false');
+
+    userEvent.click(overlay!);
+
+    expect(overlay).toHaveStyle({ opacity: 0 });
+    expect(overlay!.getAttribute('aria-hidden')).toBe('true');
+  });
 });
