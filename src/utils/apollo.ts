d572fe9e462d891f6ec237e20c0718fd41e3e810
Felipe Jung <felipemattoseu@gmail.com>
2021-03-27 13:27:20 -0400

    feat(session): add auth headers to apollo config

--- src/utils/apollo.ts
+++ src/utils/apollo.ts
@@ -1,22 +1,37 @@
+import { Session } from 'next-auth/client';
+
 import { useMemo } from 'react';
 
 import { ApolloClient, HttpLink, NormalizedCacheObject } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
 import apolloCache from './apolloCache';
 
 let apolloClient: ApolloClient<NormalizedCacheObject | null>;
 
-function createApolloClient() {
+function createApolloClient(session?: Session | null) {
+  const httpLink = new HttpLink({
+    uri: `${process.env.NEXT_PUBLIC_API_URL}/graphql`
+  });
+
+  const authLink = setContext((_, { headers }) => {
+    const authorization = session?.jwt ? `Bearer ${session.jwt}` : '';
+    return { headers: { ...headers, authorization } };
+  });
+
   return new ApolloClient({
     ssrMode: typeof window === 'undefined',
-    link: new HttpLink({ uri: 'http://localhost:1337/graphql' }),
+    link: authLink.concat(httpLink),
     cache: apolloCache
   });
 }
 
-export function initializeApollo(initialState = null) {
+export function initializeApollo(
+  initialState = null,
+  session?: Session | null
+) {
   // checks if there is already an existing instance
-  const apolloClientGlobal = apolloClient ?? createApolloClient();
+  const apolloClientGlobal = apolloClient ?? createApolloClient(session);
 
   // recovering cache data
   if (initialState) {
@@ -31,7 +46,10 @@ export function initializeApollo(initialState = null) {
   return apolloClient;
 }
 
-export function useApollo(initialState = null) {
-  const store = useMemo(() => initializeApollo(initialState), [initialState]);
+export function useApollo(initialState = null, session?: Session) {
+  const store = useMemo(() => initializeApollo(initialState, session), [
+    initialState,
+    session
+  ]);
   return store;
 }
